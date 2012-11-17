df63858cbf345e9c36a62da5063db6958c47fb0a
Amir Nissim <amirnissim@gmail.com>
2012-11-17 22:25:32 +0200

    ver 0.2

--- js/content_script.js
+++ js/content_script.js
@@ -1,33 +1,34 @@
 var EXTENSION_URL = "https://github.com/amirnissim/gmail-lighbox";
 
 var $body = $("body");
-var orig_body_html = $body.html();
+var old_content = $body.html();
 
-var $main_container = $('<div id="main-container"></div>');
-var $lightbox_container = $('<div id="lightbox-container"></div>').appendTo($main_container);
+if ($body.find("img").length > 0){  // only if page contains images (exempt pdf attachment preview)
 
-$.each($("img"), function(i, img){
-	var $img = $(img);
-	var a_wrapper = $("<a></a>").attr("href", $img.attr("src")).attr("rel", "lightbox[gmail-lightbox]");
-	$lightbox_container.append($img);
-	$img.wrap(a_wrapper);
-	$img.css("right", i*7);
-});
+	var $images = $('<div id="images"></div>');
+	$("img").each(function(i, img){
+		var $img = $(img);
+		var wrapper = $("<a></a>").attr("href", $img.attr("src")).attr("rel", "lightbox[gmail]");
+		$images.append($img);
+		$img.wrap(wrapper);
+	});
+
+	var $footer = $('<div id="footer"></div>')
+					.append("<p>" +
+						"This page was modified by " +
+						"<a href='" + EXTENSION_URL + "' target='_blank'>Gmail Lightbox</a> chrome extension." +
+						"</p>")
+					.append("<p>Take me back to the 90s and " +
+						"<a id='restore' href='#'>restore the old look<a>" +
+						"</p>");
+
+	var $new_content = $('<div id="content"></div>')
+						.append($images, $footer);
 
 
-$body.empty();
-$body.addClass("gmail-lightbox-content")
-	.append($main_container)
-	.append("<p class='gmail-lightbox-content'>" +
-		"This is modified view created by " +
-		"<a href='" + EXTENSION_URL + "' target='_blank' class='gmail-lightbox-content'>Gmail Lightbox</a>." +
-		"</p>")
-	.append("<p>" +
-		"<a class='gmail-lightbox-content' id='restore' href='#'>Restore the old look<a>" +
-		"</p>");
+	$body.empty().append($new_content).addClass("modified");
 
-$("#restore").click(function(e){
-	e.preventDefault();
-	e.stopPropagation();
-	$body.html(orig_body_html).removeClass("gmail-lightbox-content");
-});
+	$("#restore").click(function(e){
+		$body.html(old_content).removeClass("modified");
+	});
+}
